b02486e7506846658f15c892833c353814f502f8
Naama Fisher <naamaf280@gmail.com>
2025-02-16 18:50:39 +0200

    Added health check endpoint

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
-#from cgitb import handler
-from flask import Flask, render_template, request, Response
-from os import environ
-from dbcontext import db_data, db_delete, db_add, health_check
+from flask import Flask, request, Response, jsonify, render_template
 from person import Person
-import logging
+from dbcontext import db_add
 
 app = Flask(__name__)
-app.logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
-
-host_name = environ.get("HOSTNAME")
-if not health_check():
-    host_name = "no_host"
-db_host = environ.get('DB_HOST')
-backend = environ.get('BACKEND') or "http://localhost"
 
 @app.route("/")
-def main():
-    app.logger.info("Entering main route")
-    data = db_data()
-    return render_template("index.html.jinja", host_name=host_name, db_host=db_host, data=data, backend=backend)
-
-@app.route("/delete/<int:id>", methods=["DELETE"])
-def delete(id: int):
-    app.logger.info("Request to delete person with id: %s", id)
-    return db_delete(id)
+def index():
+    return render_template("index.html")
 
 @app.route("/add", methods=["PUT"])
 def add():
@@ -37,6 +17,7 @@ def add():
         return db_add(person)
     app.logger.error("Request body is empty")
     return Response(status=404)
+
 @app.route("/health")
 def health():
     health_messages = []
@@ -47,8 +28,10 @@ def health():
     except Exception as e:
         app.logger.error(f"Application health check failed: {e}")
         health_messages.append("Application: Not Healthy")
-    combined_health_status = "\\\\n".join(health_messages)
+
+    combined_health_status = "\n".join(health_messages)
     return combined_health_status
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
