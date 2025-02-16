fa36c8f2f11928f4e9572884eca87e6d5ec27000
Naama Fisher <naamaf280@gmail.com>
2025-02-16 18:50:39 +0200

    Added health check endpoint

--- app.py
+++ app.py
@@ -36,7 +36,19 @@ def add():
         person = Person(0, body["firstName"], body["lastName"], body["age"], body["address"], body["workplace"])
         return db_add(person)
     app.logger.error("Request body is empty")
-    return Response(status=404)
+@app.route("/health")
+def health():
+    health_messages = []
+    try:
+        app.logger.info("Application is running")
+        health_messages.append("Application: Healthy")
+    except Exception as e:
+        app.logger.error(f"Application health check failed: {e}")
+        health_messages.append("Application: Not Healthy")
+
+    combined_health_status = "\\n".join(health_messages)
+    return combined_health_status
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
